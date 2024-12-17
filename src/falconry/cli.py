d285471def40c1bccc2085c929ae3903f157f6ce
fnechans <filip.nechansky@protonmail.com>
2024-12-17 13:25:28 +0100

    Add proper docstring (#12)
    * Add proper docstrings
    
    * Make some functions in job/manager internal
    
    ---------
    
    Co-authored-by: Filip Nechansky <filip.nechansky@cern.ch>

--- src/falconry/cli.py
+++ src/falconry/cli.py
@@ -14,10 +14,24 @@ class InputState(Enum):
     TIMEOUT = 1
 
 
-def input_checker(validOptions: Dict[str, str], timeout: int = 60,
-                  message: str = "Following options available:",
-                  silent: bool = False) -> Tuple[InputState, Optional[str]]:
+def input_checker(
+    validOptions: Dict[str, str],
+    timeout: int = 60,
+    message: str = "Following options available:",
+    silent: bool = False,
+) -> Tuple[InputState, Optional[str]]:
+    """Helper function to get input from user
 
+    Arguments:
+        validOptions (Dict[str, str]): dictionary of valid options
+        timeout (int, optional): timeout in seconds. Defaults to 60.
+        message (str, optional): message to print before options.
+        Defaults to "Following options available:".
+        silent (bool, optional): silent mode. Defaults to False.
+
+    Returns:
+        Tuple[InputState, Optional[str]]: returns the state of the input
+    """
     if message != "" and not silent:
         log.info(message)
     for opt, desc in validOptions.items():
