4ae834a376b0a4b2a4c489905eff2a80cd0fdbad
Filip Nechansky <nechansky.filip@gmail.com>
2024-12-17 14:28:11 +0100

    mock ijson/htcondor in docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ import sys
 from os.path import abspath, dirname, join
 sys.path.insert(0, abspath(join(dirname(__file__), '../src')))
 
+autodoc_mock_imports = ['ijson', 'htcondor']
+
 
 # -- General configuration ------------------------------------------------
 
